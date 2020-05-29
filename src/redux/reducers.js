e116c38378ae361c1b623602760535c9b8d70f02
lalitsheoran <lalit_sheoran@outlook.com>
2020-05-29 21:53:36 +0530

    fixes

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -23,10 +23,10 @@ export const reducers=(state=initialState,action)=>{
             flags[Object.keys(action.payload[0])[0]]=true
             return {...state,currentInv:{...newList,...action.payload[0]},amount:action.payload[1],invFlags:flags}
         case INV_DONE:
-            let list=state.totalInv
+            // let list=state.totalInv
             let flags2=state.invFlags
             flags2={Real_Estate:false,Savings_Account:false,Stocks:false,PPF:false,Mutual_Funds:false,Debentures:false,Gold:false,Fixed_Deposit:false}
-            return {...state,totalInv:[...list,action.payload],currentInv:{},timeFlag:false,invFlags:flags2}
+            return {...state,totalInv:[action.payload],currentInv:{},timeFlag:false,invFlags:flags2}
         case TIME:
             return {...state,time:action.payload,timeFlag:true}
         default:
